8ca6d62d01287544abb67b840928648961e0e356
usbbush <jeanbienvenusb@gmail.com>
2022-06-14 15:12:26 +0200

    Implement javascript menu hamburger

---
+++ script.js
@@ -0,0 +1,36 @@
+const closeMenuIcon = document.querySelector('.close');
+const openMenuIcon = document.querySelector('.bars');
+const menu = document.querySelector('#mobile-menu');
+const body = document.querySelector('body');
+const menuItems = document.querySelectorAll('.mobile-menu .list-item');
+
+function init() {
+    body.classList.remove('overflow-y');
+    document.querySelector('#headline').classList.toggle('blur');
+    document.querySelector('#toolbar').classList.toggle('blur');
+    menu.classList.toggle('d-block');
+    menu.classList.toggle('d-none');
+}
+
+function openMenu() {
+    init();
+    body.classList.toggle('overflow-y')
+}
+
+function closeMenu() {
+    init();
+}
+
+closeMenuIcon.addEventListener('click', () => {
+    closeMenu();
+
+});
+openMenuIcon.addEventListener('click', () => {
+    openMenu();
+});
+
+menuItems.forEach(function (item) {
+    item.addEventListener('click', () => {
+        closeMenu();
+    })
+});
