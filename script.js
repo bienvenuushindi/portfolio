ac5dfcf5975da92f1bf80eeba6a7bc3e126b5877
Aleksandra Ujvari <ujvari65@gmail.com>
2022-06-15 19:39:42 +0200

    Add some JS for project and fix some linters errors.

--- script.js
+++ script.js
@@ -3,11 +3,41 @@ const openMenuIcon = document.querySelector('.bars');
 const menu = document.querySelector('#mobile-menu');
 const body = document.querySelector('body');
 const menuItems = document.querySelectorAll('.mobile-menu .list-item');
-const projectDetailsButtons=document.querySelectorAll('.project-btn');
-const modalBox=document.querySelector('.modal');
-const projects={
+const projectDetailsButtons = document.querySelectorAll('.project-btn');
+const modalBox = document.querySelector('.modal');
+const projects = document.querySelectorAll('card');
+const card1 = {
+  name: '',
+  lenguage: '',
+  image: '',
+  live: '',
+  source: '',
+};
 
-}
+const card2 = {
+  name: '',
+  lenguage: '',
+  image: '',
+  live: '',
+  source: '',
+};
+
+const card3 = {
+  name: '',
+  lenguage: '',
+  image: '',
+  live: '',
+  source: '',
+};
+
+const card4 = {
+  name: '',
+  lenguage: '',
+  image: '',
+  live: '',
+  source: '',
+};
+
 function init() {
   body.classList.remove('overflow-y');
   document.querySelector('#headline').classList.toggle('blur');
@@ -38,9 +68,21 @@ menuItems.forEach((item) => {
     closeMenu();
   });
 });
-projectDetailsButtons.forEach((btn)=>{
-    btn.addEventListener('click',()=>{
-        console.log("clicked");
-        modalBox.classList.remove('d-none');
-    });
+projectDetailsButtons.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    console.log('clicked');
+    modalBox.classList.remove('d-none');
+  });
+});
+
+modalBox.forEach((modal) => {
+  modal.addEventListener('click', () => {
+    modalBox.classList.add('active');
+  });
+});
+
+modalBox.forEach((modal) => {
+  modal.addEventListener('click', () => {
+    modalBox.classList.remove('active');
+  });
 });
