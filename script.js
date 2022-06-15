2231b43074c65c2699ab8be26a51d843fcecf4e8
usbbush <jeanbienvenusb@gmail.com>
2022-06-15 17:04:21 +0200

    Display modal on project details button clicked

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const openMenuIcon = document.querySelector('.bars');
 const menu = document.querySelector('#mobile-menu');
 const body = document.querySelector('body');
 const menuItems = document.querySelectorAll('.mobile-menu .list-item');
+const projectDetailsButtons=document.querySelectorAll('.project-btn');
+const modalBox=document.querySelector('.modal');
+const projects={
 
+}
 function init() {
   body.classList.remove('overflow-y');
   document.querySelector('#headline').classList.toggle('blur');
@@ -33,4 +37,11 @@ menuItems.forEach((item) => {
   item.addEventListener('click', () => {
     closeMenu();
   });
+});
+
+projectDetailsButtons.forEach((btn)=>{
+    btn.addEventListener('click',()=>{
+        console.log("clicked");
+        modalBox.classList.remove('d-none');
+    });
 });
